9af9fb2ea6900421cb1f50ed52c7c2b284cd3960
Ian Bateman <ian.h.bateman.18@dartmouth.edu>
2016-07-11 22:45:30 -0400

    added citations

--- app/server.js
+++ app/server.js
@@ -1,10 +1,12 @@
-// example bot
+// bot imports
 import botkit from 'botkit';
 import Yelp from 'yelp';
 
+// log starting statement
 console.log('starting bot');
 
 // initialize Yelp
+// structure taken in part from https://github.com/olalonde/node-yelp
 const opts = ({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
@@ -14,17 +16,13 @@ const opts = ({
 
 const yelp = new Yelp(opts);
 
-// yelp test
-console.log(yelp.search({
-  term: 'food',
-  location: 'Montreal',
-}));
-
+// GIVEN AT https://github.com/dartmouth-cs52/slackattack
 // botkit controller
 const controller = botkit.slackbot({
   debug: false,
 });
 
+// GIVEN AT https://github.com/dartmouth-cs52/slackattack
 // initialize slackbot
 const slackbot = controller.spawn({
   token: process.env.SLACK_BOT_TOKEN,
@@ -34,6 +32,7 @@ const slackbot = controller.spawn({
   if (err) { throw new Error(err); }
 });
 
+// GIVEN AT https://github.com/dartmouth-cs52/slackattack
 // prepare webhook
 // for now we won't use this but feel free to look up slack webhooks
 controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
@@ -42,11 +41,13 @@ controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
   });
 });
 
+// GIVEN AT https://github.com/dartmouth-cs52/slackattack
 // outgoing webhook
 controller.on('outgoing_webhook', (bot, message) => {
   bot.replyPublic(message, 'yeah yeah');
 });
 
+// MODIFIED FROM GIVEN AT https://github.com/dartmouth-cs52/slackattack
 // hello response
 controller.hears(['hello', 'hi', 'howdy', 'hey', 'hola', 'yo', 'sup'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
   bot.api.users.info({ user: message.user }, (err, res) => {
@@ -68,6 +69,7 @@ controller.hears(['help'], ['direct_message', 'direct_mention', 'mention'], (bot
   bot.reply(message, 'That is about it!');
 });
 
+// makes recommendation
 const giveResults = (response, convo, place, type) => {
   convo.say('Ok! One sec. Pulling up results.');
   convo.say(`You input ${place} as your location and ${type} as your preferred food type.`);
@@ -75,12 +77,15 @@ const giveResults = (response, convo, place, type) => {
   yelp.search({ term: type, location: place })
     .then((data) => {
       let num = 0;
+      // MODIFIED FROM GIVEN AT https://github.com/dartmouth-cs52/slackattack
       data.businesses.forEach(business => {
         if (num < 1) {
           convo.say('Here is my suggestion:');
           convo.say(`You might like: ${business.name}`);
           convo.say(`Its rating is: ${business.rating}/5`);
           const attch = {
+            // structure taken in part from https://github.com/howdyai/botkit
+            // structure taken in part from https://api.slack.com/docs/message-attachments
             attachments: [
               {
                 fallback: `My recommendation was ${business.name}, but unfortunately the attachment is not rendering`,
@@ -105,6 +110,8 @@ const giveResults = (response, convo, place, type) => {
     });
 };
 
+// structure taken in part from https://github.com/howdyai/botkit
+// asks user location
 const askDeliver = (response, convo, type) => {
   convo.ask('Where are you?', (reply, conversation) => {
     const userLocation = reply.text;
@@ -114,6 +121,8 @@ const askDeliver = (response, convo, type) => {
   });
 };
 
+// structure taken in part from https://github.com/howdyai/botkit
+// asks the type of food wanted
 const askType = (response, convo) => {
   convo.ask('What type of food are you interested in?', (reply, conversation) => {
     convo.say('Ok.');
@@ -123,6 +132,8 @@ const askType = (response, convo) => {
   });
 };
 
+// structure taken in part from https://github.com/howdyai/botkit
+// start of hungry conversation
 controller.hears(['hungry'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
   bot.startConversation(message, (err, convo) => {
     convo.ask('Would you like food recommendations near you?', [
@@ -153,6 +164,7 @@ controller.hears(['hungry'], ['direct_message', 'direct_mention', 'mention'], (b
   });
 });
 
+// random message response
 controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
   bot.reply(message, 'You are coming in all broken up, can you say that again?');
 });
